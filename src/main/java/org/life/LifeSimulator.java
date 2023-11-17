7c7a5f2c03e53be94295ae9ac6d0cb805b2c4935
KajaInf <kaja.michalik@student.uj.edu.pl>
2023-11-17 02:25:31 +0100

    lab03 working really well

--- src/main/java/org/life/LifeSimulator.java
+++ src/main/java/org/life/LifeSimulator.java
@@ -3,9 +3,19 @@ package org.life;
 public class LifeSimulator {
 
   public static void main(String[] args) {
-    Board board = new Board(10, 10);
-    Organism organism = new Organism(100,board);
-    board.addOrganism(organism, 5, 5);
-    organism.move();
+    Board board = new Board(25, 125);
+    Organism organism1 = new Organism(100,board);
+    Jumper organism2 = new Jumper(20, board);
+    board.addOrganism(organism1, 5, 5);
+    board.addOrganism(organism2, 3,4);
+    for (int i = 0; i < 20; i++){
+      organism1.move();
+
+      System.out.println("Org1 : ");
+      organism2.move();
+
+      System.out.println("Org2 : " );
+    }
+
   }
 }
